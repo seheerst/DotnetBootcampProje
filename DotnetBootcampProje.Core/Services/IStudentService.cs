51fa593852561119321424cb7a43812e0b63f48b
seheerst <seherst9@gmail.com>
2023-12-29 18:55:29 +0300

    created service for all entities

---
+++ DotnetBootcampProje.Core/Services/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using DotnetBootcampProje.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Core.Services
+{
+    public interface IStudentService : IGenericService<Student>
+    {
+    }
+}
