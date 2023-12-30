f0e4e855a838ee6c80c0b871d65b63fdb1b81eae
seheerst <seherst9@gmail.com>
2023-12-30 20:14:41 +0300

    configuration dbContext

--- DotnetBootcampProje.Api/Program.cs
+++ DotnetBootcampProje.Api/Program.cs
@@ -1,15 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using DotnetBootcampProje.Core.Repositories;
+using DotnetBootcampProje.Core.UnitOfWork;
+using DotnetBootcampProje.Repository;
+using DotnetBootcampProje.Repository.Repositories;
+using DotnetBootcampProje.Repository.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    });
+});
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
