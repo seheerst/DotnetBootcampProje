cf406acbce83eddb5194101ad42bc5713c427fe7
seheerst <seherst9@gmail.com>
2023-12-30 20:17:52 +0300

    created db context

---
+++ DotnetBootcampProje.Repository/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using DotnetBootcampProje.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
