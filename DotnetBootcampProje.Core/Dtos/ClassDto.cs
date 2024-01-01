2124ebff7e045bee336d870a89c140092b907cd5
seheerst <seherst9@gmail.com>
2024-01-01 19:51:26 +0300

    created dtos for all entities

---
+++ DotnetBootcampProje.Core/Dtos/ClassDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Core.Dtos
+{
+    public class ClassDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ DotnetBootcampProje.Core/Dtos/StudentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Core.Dtos
+{
+    public class StudentDto
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
