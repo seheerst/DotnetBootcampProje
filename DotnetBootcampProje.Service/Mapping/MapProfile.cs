f063f158374413ae527ce0f66496dea7515b8309
seheerst <seherst9@gmail.com>
2024-01-01 19:52:45 +0300

    created maping for all dtos

---
+++ DotnetBootcampProje.Service/Mapping/MapProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DotnetBootcampProje.Core.Dtos;
+using DotnetBootcampProje.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Class, ClassDto>().ReverseMap();
+            CreateMap<Teacher, TeacherDto>().ReverseMap();
+            CreateMap<Student, StudentDto>().ReverseMap();
+        }
+    }
+}
