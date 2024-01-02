6381cb0e31733f43aa8fbee9fcda724f4fc822b9
seheerst <seherst9@gmail.com>
2024-01-02 22:40:08 +0300

    created service for class

---
+++ DotnetBootcampProje.Service/Services/ClassService.cs
@@ -0,0 +1,19 @@
+﻿using DotnetBootcampProje.Core.Models;
+using DotnetBootcampProje.Core.Repositories;
+using DotnetBootcampProje.Core.Services;
+using DotnetBootcampProje.Core.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Service.Services
+{
+    public class ClassService : GenericService<Class>, IClassService
+    {
+        public ClassService(IGenericRepository<Class> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+    }
+}
