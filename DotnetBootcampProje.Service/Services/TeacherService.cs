dfa93b2b9d3d5c712cc43a85f352721a8c8c9413
seheerst <seherst9@gmail.com>
2024-01-02 22:40:23 +0300

    created service for teacher

---
+++ DotnetBootcampProje.Service/Services/TeacherService.cs
@@ -0,0 +1,19 @@
+﻿using DotnetBootcampProje.Core.Models;
+using DotnetBootcampProje.Core.Repositories;
+using DotnetBootcampProje.Core.Services;
+using DotnetBootcampProje.Core.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Service.Services
+{
+    public class TeacherService : GenericService<Teacher>, ITeacherService
+    {
+        public TeacherService(IGenericRepository<Teacher> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+    }
+}
