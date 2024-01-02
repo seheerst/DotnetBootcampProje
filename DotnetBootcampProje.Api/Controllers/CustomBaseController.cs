42a861107cd5416b3fc841d6d0d0941f5d8e146d
seheerst <seherst9@gmail.com>
2024-01-02 22:39:27 +0300

    created base controller

---
+++ DotnetBootcampProje.Api/Controllers/CustomBaseController.cs
@@ -0,0 +1,28 @@
+﻿using DotnetBootcampProje.Core.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetBootcampProje.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.StatusCode == 204)
+            {
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+            }
+
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
