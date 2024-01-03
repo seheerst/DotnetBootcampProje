41a0aa3ffe766b36e5dad991fc12d2a232282bd5
seheerst <seherst9@gmail.com>
2024-01-03 20:59:40 +0300

    service implementation for student

---
+++ DotnetBootcampProje.Service/Services/StudentService.cs
@@ -0,0 +1,19 @@
+﻿using DotnetBootcampProje.Core.Models;
+using DotnetBootcampProje.Core.Repositories;
+using DotnetBootcampProje.Core.Services;
+using DotnetBootcampProje.Core.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Service.Services
+{
+    public class StudentService : GenericService<Student>, IStudentService
+    {
+        public StudentService(IGenericRepository<Student> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+    }
+}
