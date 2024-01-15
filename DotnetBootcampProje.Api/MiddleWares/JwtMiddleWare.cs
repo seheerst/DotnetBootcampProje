c78f7fb22ef98f88944cdf73b4988bcb97451b6a
seheerst <seherst9@gmail.com>
2024-01-15 23:15:14 +0300

    created middleware for jwt and exception handler

---
+++ DotnetBootcampProje.Api/MiddleWares/JwtMiddleWare.cs
@@ -0,0 +1,38 @@
+﻿using DotnetBootcampProje.Core.Services;
+using DotnetBootcampProje.Service.Authorization.Abstraction;
+
+namespace DotnetBootcampProje.Api.MiddleWares
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, ITeacherService teacherService, IJwtAuthenticationManager iJwtAuthenticationManager)
+        {
+            var authorizationHeader = context.Request.Headers["Authorization"].FirstOrDefault();
+            string token = null;
+
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                var parts = authorizationHeader.Split(" ");
+                if (parts.Length > 1)
+                {
+                    token = parts[parts.Length - 1];
+                }
+            }
+
+            var teacherId = iJwtAuthenticationManager.ValidateJwtToken(token);
+            if (teacherId != null)
+            {
+                context.Items["User"] = teacherService.GetById(teacherId.Value).Result;
+            }
+
+            await _next(context);
+        }
+    }
+}
