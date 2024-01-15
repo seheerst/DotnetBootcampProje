b814b5a6c5dd9ac5a008a5b8398c1dcf68a2b62a
seheerst <seherst9@gmail.com>
2024-01-15 23:18:19 +0300

    created repo module

---
+++ DotnetBootcampProje.Api/Modules/RepoModuleService.cs
@@ -0,0 +1,41 @@
+﻿using DotnetBootcampProje.Core.Repositories;
+using DotnetBootcampProje.Core.UnitOfWork;
+using DotnetBootcampProje.Repository.Repositories;
+using DotnetBootcampProje.Repository.UnitOfWork;
+using DotnetBootcampProje.Repository;
+using DotnetBootcampProje.Service.Mapping;
+using System.Reflection;
+using Autofac;
+using DotnetBootcampProje.Service;
+using DotnetBootcampProje.Core.Services;
+using Module = Autofac.Module;
+
+namespace DotnetBootcampProje.Api.Modules
+{
+    public class RepoModuleService : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            //Generic olduğu için bu şekilde belirttik.
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(GenericService<>)).As(typeof(IGenericService<>)).InstancePerLifetimeScope();
+            //Generic değil o yüzden type olarak ekledik.
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+            //Bunun anlamı da bulunduğun klasörde ara demek
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            //Repo katmanındaki herhangi bir classı typeof içine verebiliriz. Bu şekilde diğerlerini kendisi bulacaktır.
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            //Service katmanındaki herhangi bir classı typeof içine verebiliriz. Bu şekilde diğerlerini kendisi bulacaktır.
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            //InstancePerLifetimeScope => Scope a karşılık gelir.
+            //InstancePerDependency => Transient a karşılık gelir.
+            //Burada şunu demek istiyoruz, bu verilen Assembly'lere git ve sonu Repository ile bitenlerin Scope olarak instance'ını al. builder.Services.AddScoped<IProductRepository, ProductRepository>(); => bu yazdığımızı tüm repository yerine tek tek yazmak yerine bu şekilde bütün hepsi için Scope olarak instance alıyoruz.
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+        }
+    }
+}
