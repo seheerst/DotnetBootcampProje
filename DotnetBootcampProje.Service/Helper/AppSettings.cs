f8b4209e13e460fddc70904151de1728d4b9f60e
seheerst <seherst9@gmail.com>
2024-01-15 23:21:21 +0300

    created class for app settings

---
+++ DotnetBootcampProje.Service/Helper/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Service.Helper
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; } = "dotnet_panel_key_value_here_secret";
+
+        public int RefreshTokenTTL { get; set; }
+    }
+}
