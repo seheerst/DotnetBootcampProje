e48eeececb0e01c12cc8a3a65baa132ebb431111
seheerst <seherst9@gmail.com>
2024-01-15 23:11:43 +0300

    created dtos for authentication request and response

---
+++ DotnetBootcampProje.Core/Dtos/Authentication/AuthRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Core.Dtos.Authentication
+{
+
+    public class AuthRequestDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ DotnetBootcampProje.Core/Dtos/Authentication/AuthResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Core.Dtos.Authentication
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; }
+        public TeacherDto Teacher { get; set; }
+    }
+}
