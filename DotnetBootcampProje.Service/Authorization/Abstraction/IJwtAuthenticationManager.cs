a9fc9ac0f79e0f968d57f2c0b3b049fd042224ac
seheerst <seherst9@gmail.com>
2024-01-15 23:12:52 +0300

    created interface for jwt

---
+++ DotnetBootcampProje.Service/Authorization/Abstraction/IJwtAuthenticationManager.cs
@@ -0,0 +1,15 @@
+﻿using DotnetBootcampProje.Core.Dtos.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBootcampProje.Service.Authorization.Abstraction
+{
+    public interface IJwtAuthenticationManager
+    {
+        AuthResponseDto Authenticate(string email, string password);
+        int? ValidateJwtToken(string token);
+    }
+}
